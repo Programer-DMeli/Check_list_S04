e6b8fbbfbdc7b1b96dc26b19c0e1226b3088dbee
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-16 00:30:24 -0500

    Feat: Implementacion de lista de tareas con Componentes basicos

--- app/src/main/java/com/carbajal/check_list/ListaTareas.kt
+++ app/src/main/java/com/carbajal/check_list/ListaTareas.kt
@@ -1,12 +1,86 @@
 package com.carbajal.check_list
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
+data class Tarea (
+    val id: Int,
+    val nombre: String,
+    val completada: Boolean = false
 
+)
 
 @Composable
-fun ListaTareas(){
+fun ListaTareas(
+    tarea: Tarea,
+    onEliminar: () -> Unit,
+    onCambiarEstado: (Boolean) -> Unit
+){
+    //Implementamos el componente Card
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.weight(1f)
+            ) {
+                Checkbox(
+                    checked = tarea.completada,
+                    onCheckedChange = {
+                        onCambiarEstado(it)
+
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = tarea.nombre,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 15.dp))
+            }
+            Button(
+                onClick = onEliminar
+            ) {
+                Text("Eliminar")
+            }
+        }
+    }
+}
+
+@Composable
+fun PantallaTareas(){
+
+    //declaramos nuestras variables
+    var textotarea by remember { mutableStateOf("") }
+    var contadorId by remember { mutableStateOf(1)}
+    //variable de almacenamiento
+    var listaTareas = remember { mutableStateListOf<Tarea>() }
+
 
 
 }
