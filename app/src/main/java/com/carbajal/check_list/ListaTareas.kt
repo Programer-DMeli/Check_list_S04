3339e373fc7067eb653a4017ab3b83b9032a5f2b
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-16 06:41:08 -0500

    feat: Implementacion de PantallaTareas con column y lazyColumn

--- app/src/main/java/com/carbajal/check_list/ListaTareas.kt
+++ app/src/main/java/com/carbajal/check_list/ListaTareas.kt
@@ -1,8 +1,11 @@
 package com.carbajal.check_list
 
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,6 +14,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,8 +23,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.items
 
 
 data class Tarea (
@@ -31,7 +39,7 @@ data class Tarea (
 )
 
 @Composable
-fun ListaTareas(
+fun ItemTarea(
     tarea: Tarea,
     onEliminar: () -> Unit,
     onCambiarEstado: (Boolean) -> Unit
@@ -57,7 +65,7 @@ fun ListaTareas(
 
                     }
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.width(8.dp))
 
                 Text(text = tarea.nombre,
                     style = MaterialTheme.typography.bodyLarge,
@@ -77,10 +85,77 @@ fun PantallaTareas(){
 
     //declaramos nuestras variables
     var textotarea by remember { mutableStateOf("") }
-    var contadorId by remember { mutableStateOf(1)}
+    var contadorId by remember { mutableIntStateOf(1)}
     //variable de almacenamiento
-    var listaTareas = remember { mutableStateListOf<Tarea>() }
+    val listaTareas = remember { mutableStateListOf<Tarea>() }
 
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp)
 
+    ) {
+        Text(text = "LISTA DE TAREAS TECBOOK",
+            fontWeight = FontWeight.Bold,
+            )
 
+        Spacer(modifier = Modifier.height(8.dp))
+
+        //Pedimos un texto de entrada
+
+        OutlinedTextField(
+            value = textotarea,
+            onValueChange = {textotarea = it},
+            label = {Text(text = "INgrese una tarea")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = {
+
+                //Logica
+                if (textotarea.isNotBlank()){
+                    listaTareas.add(
+                        Tarea (
+                            id = contadorId,
+                            nombre = textotarea,
+                        )
+                    )
+                    contadorId++
+                    textotarea = ""
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Agregar Tarea",
+                fontWeight = FontWeight.Bold)
+
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(text = "Total tareas: ${listaTareas.size}",
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn{
+            items(listaTareas, key = {it.id}){tarea ->
+                ItemTarea(
+                    tarea = tarea,
+                    onEliminar = {
+                        listaTareas.remove(tarea)
+                    },
+                    onCambiarEstado = { completada ->
+                        val index = listaTareas.indexOf(tarea)
+                        if(index != -1){
+                            listaTareas[index] = listaTareas[index].copy(completada = completada)
+                        }
+                    }
+
+                )
+            }
+        }
+    }
 }
